cb78cdc49944baee51189f6a4e8ab84dc7aee825
zr <zr@test.com>
2022-08-29 21:36:23 +0800

    中间件改进

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,4 +1,6 @@
-const { createUser, getUserInfo} = require('../service/user.service')
+const { userRegisterError } = require('../constant/error.type')
+const { createUser, getUserInfo } = require('../service/user.service')
+
 class UserController {
     async register(ctx, next) {
         console.log(ctx.request.body)
@@ -7,19 +9,24 @@ class UserController {
         const user_name = ctx.request.body.user_name
         const password = ctx.request.body.password
         // 2. 操作数据库
-       
 
-        const res = await createUser(user_name, password)
-        console.log(res)
-        // 3. 返回结果
-        ctx.body = {
-            code:0,
-            message:"用户注册成功",
-            result:{
-                id:res.id,
-                user_name:res.user_name
+        try {
+            const res = await createUser(user_name, password)
+            console.log(res)
+            // 3. 返回结果
+            ctx.body = {
+                code: 0,
+                message: "用户注册成功",
+                result: {
+                    id: res.id,
+                    user_name: res.user_name
+                }
             }
+        } catch (err) {
+            console.log(err)
+            ctx.app.emit("error", userRegisterError,ctx)
         }
+
     }
     async login(ctx, next) {
         ctx.body = '登陆成功'
--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -1,23 +1,33 @@
-const {getUserInfo } = require("../service/user.service")
-const {userAlreadyExisted, userFormatError}  = require('../constant/error.type')
-const userValidator =  async(ctx,next)=>{
-    const {user_name, password} = ctx.request.body
+const { getUserInfo } = require("../service/user.service")
+const { userAlreadyExisted, userFormatError, userRegisterError } = require('../constant/error.type')
+const userValidator = async (ctx, next) => {
+    const { user_name, password } = ctx.request.body
 
-    if(!user_name ||!password){
-        console.error("用户名或密码为空",ctx.request.body)
+    if (!user_name || !password) {
+        console.error("用户名或密码为空", ctx.request.body)
         ctx.app.emit('error', userFormatError, ctx)
-         
-        
         return
     }
-   
+
     await next()
 }
-const verifyUser = async(ctx,next)=>{
-    const {user_name} = ctx.request.body
- if(getUserInfo({user_name})){
-        console.error("用户存在",ctx.request.body)
-        ctx.app.emit("error",userAlreadyExisted,ctx)
+const verifyUser = async (ctx, next) => {
+    const { user_name } = ctx.request.body
+    //  if(await getUserInfo({user_name})){
+    //         console.error("用户存在",ctx.request.body)
+    //         ctx.app.emit("error",userAlreadyExisted,ctx)
+    //         return
+    //     }
+    try {
+        const res = await getUserInfo({ user_name })
+        if (res) {
+            console.error("用户存在", {user_name})
+            ctx.app.emit("error", userAlreadyExisted, ctx)
+            return
+        }
+    } catch (err) {
+        console.error("获取用户信息错误", {user_name})
+        ctx.app.emit("error", userRegisterError, ctx)
         return
     }
     await next()
