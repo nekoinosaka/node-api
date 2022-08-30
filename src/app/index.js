1418fa165d972a9b7cc83f1aa7769c71493d0dad
zr <zr@test.com>
2022-08-30 19:10:43 +0800

    路由自动加载

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,11 @@
 const Koa = require("koa")
 const KoaBody = require('koa-body')
 const errHandler = require('./errorHandler')
-const userRouter = require("../router/user.route")
-
+const router = require("../router")
 const app = new Koa()
 app.use(KoaBody())
-app.use(userRouter.routes()) 
-
+app.use(router.routes())
+app.use(router.allowedMethods())
 // 统一的错误处理
-app.on("error",errHandler)
+app.on("error", errHandler)
 module.exports = app
--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,12 +1,12 @@
 const Router = require('koa-router')
 const { auth } = require("../middleware/auth.middleware")
 const { userValidator, verifyUser, crpytPassword, verifyLogin } = require("../middleware/user.middleware")
-const { changePassword,register, login } = require("../controller/user.controller")
+const { changePassword, register, login } = require("../controller/user.controller")
 
 const router = new Router({ prefix: '/users' })
 //  注册接口
 router.post('/register', userValidator, verifyUser, crpytPassword, register)
 router.post('/login', userValidator, verifyLogin, login)
 // 修改密码
-router.patch('/', auth, crpytPassword,changePassword)
+router.patch('/', auth, crpytPassword, changePassword)
 module.exports = router
